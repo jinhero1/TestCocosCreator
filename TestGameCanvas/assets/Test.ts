b57eee25b2fe0305c5b0e58dfe4fb5592683e6db
Jin Chen <jin7157@yahoo.com.tw>
2024-06-13 22:18:26 +0800

    Test security bug

--- TestGameCanvas/assets/Test.ts
+++ TestGameCanvas/assets/Test.ts
@@ -3,11 +3,14 @@ const { ccclass, property } = _decorator;
 
 @ccclass("Test")
 export class Test extends Component {
+    const username = "admin";
+    const password = "123456";
+
     start() {
         const bug1 = this.writeReliabilityBug();
         console.log("Trace bug1: ", bug1);
 
-        const bug2 = this.writeSecurityBug();
+        const bug2 = this.writeSecurityHotspots();
         console.log("Trace bug2: ", bug2);
     }
 
@@ -18,7 +21,7 @@ export class Test extends Component {
         }
     }
 
-    private writeSecurityBug(): number {
+    private writeSecurityHotspots(): number {
         return Math.random();
     }
 }
